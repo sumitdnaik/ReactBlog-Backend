d961553fc9730c686f0f0ce83c27d4bfe7760e35
prakash-nale <prakash.nale@snapav.com>
2018-10-28 19:15:23 +0530

    get profile API

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,29 @@ const errorUtils = require('../utils/errorUtils');
 const JWTAuthMiddleware = require("../middlewares/JWTAuthentication");
 
 
+router.post("/getProfile", function(req, res){
+      const db = getDb();
+      const userCollection = db.collection('users');
+      let userProfileObj = req.body;
+      console.log('user email '+userProfileObj.email);
+      res.setHeader('Content-Type', 'application/json');
+      userCollection.find({'email': userProfileObj.email}).toArray(function(err, docs){
+        console.log('user data '+docs);
+        console.log('user data '+err);
+      if(err){
+        errorUtils.errorRes(res, 'Error fetching user data');
+        return;
+      }
+      else {
+        res.json({
+          message: "Profile retrieved successfully",
+          stories: docs,
+          status: true
+        });
+      }
+  });
+});
+
 router.post('/saveProfile', JWTAuthMiddleware, function (req,res) {
       const db = getDb();
       const userCollection = db.collection('users');
